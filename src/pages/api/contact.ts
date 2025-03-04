62184eb089117c5197f11a5aaf6d016646a9f755
joshuaposton <140981423+joshuaposton@users.noreply.github.com>
2025-03-04 16:23:00 -0600

    Update contact.ts

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -18,8 +18,8 @@ type ResponseData = {
 };
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
+  host: "smtp.gmail.com",
+  port: 465,
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
